77277af0dab9e0a68c109b5a286024e6b3b0c942
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-12-14 00:00:40 -0600

    fix: add xargs for Windows

---
+++ bin/xargs.cmd
@@ -0,0 +1,39 @@
+:: https://github.com/DoctorLai/BatchUtils/blob/23d55a27d4e939893c43709e5eecd275b3f075f5/xargs.cmd#L1
+
+@echo off
+:: example:   git branch | grep -v "develop" | xargs git branch -D
+:: example    xargs -a input.txt echo
+:: https://helloacm.com/simple-xargs-batch-implementation-for-windows/
+setlocal enabledelayedexpansion
+
+set args=
+set file='more'
+
+:: read from file
+if "%1" == "-a" (
+    if "%2" == "" (
+        echo Correct Usage: %0 -a Input.txt command
+        goto end
+    )
+    set file=%2
+    shift
+    shift
+    goto start
+)
+
+:: read from stdin
+set args=%1
+shift
+
+:start
+    if [%1] == [] goto start1
+    set args=%args% %1
+    shift
+    goto start
+
+:start1
+    for /F "tokens=*" %%a in (!file!) do (
+        %args% %%a
+    )
+
+:end
